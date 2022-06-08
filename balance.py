1908926ba2b717960b9b4d0eb13026bef05dffb5
MarWo22 <83605811+MarWo22@users.noreply.github.com>
2022-06-08 12:26:18 +0200

    Added script to balance data

---
+++ balance.py
@@ -0,0 +1,77 @@
+import re
+import csv
+import random
+
+def getAnnotations(filePath):
+    with open(filePath, 'r') as in_file:
+        stripped = (line.strip() for line in in_file)
+        lines = (re.split(' +', line)[1:3] for line in stripped if line)
+        lines = list(lines)
+    
+    return lines[2:]
+
+def getTimeseries(filePath):
+
+    time_series_1 = []
+    time_series_2 = []
+
+    with open(filePath) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            next(csv_reader, None)
+            for row in csv_reader:
+                time_series_1.append(float(row[1]))
+                time_series_2.append(float(row[2]))
+    
+    return time_series_1, time_series_2
+
+def createStaticWindows(annotations, time_series_1, time_series_2):
+
+    annotatedWindows = []
+
+    for heartbeat in annotations:
+        low = int(heartbeat[0]) - 40
+        high = int(heartbeat[0]) + 40
+        time_series_1_window = time_series_1[low:high]
+        time_series_2_window = time_series_2[low:high]
+        annotatedWindows.append([heartbeat[1], time_series_1_window, time_series_2_window])
+
+    return annotatedWindows
+
+
+def rebalance(annotatedWindows):
+
+    normal_beats = [] 
+    pathological_beats = []
+    balanced_beats = []
+
+    for window in annotatedWindows:
+        if window[0] == 'N':
+            normal_beats.append(window)
+        else:
+            pathological_beats.append(window)
+    
+
+    if normal_beats > pathological_beats:
+        balanced_pathological_beats = pathological_beats.copy()
+        for _ in range(len(normal_beats) - len(pathological_beats)):
+            balanced_pathological_beats.append(random.choice(pathological_beats))
+        balanced_beats = normal_beats + balanced_pathological_beats
+    else:
+        balanced_normal_beats = normal_beats.copy()
+        for _ in range(len(pathological_beats) - len(normal_beats)):
+            balanced_normal_beats.append(random.choice(normal_beats))
+        balanced_beats = normal_beats + balanced_normal_beats 
+    
+    random.shuffle(balanced_beats)
+    return balanced_beats
+
+
+def main():
+    annotations = getAnnotations("annotations.txt")
+    time_series_1, time_series_2 = getTimeseries("100_filtered.csv")
+    annotatedWindows = createStaticWindows(annotations, time_series_1, time_series_2)
+    balanced_dataset = rebalance(annotatedWindows)
+    print(balanced_dataset[0])
+
+
+main()
--- filter.py
+++ filter.py
@@ -1,11 +1,16 @@
 import sys
 import csv
 from scipy import signal
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Call the script with two arguments, the first one being the path to the input CSV file, and the second one the output path
 
 if(len(sys.argv) < 3):
     exit(0)
+
+
+
 
 
 input = sys.argv[1]
@@ -26,9 +31,13 @@ with open(input) as csv_file:
             time_series_2.append(int(row[2]))
     print("Read input")
 
-b, a = signal.butter(3, [0.4, 45], 'bandpass', fs=360)
-filtered_time_series_1 = signal.lfilter(b, a, time_series_1)
-filtered_time_series_2 = signal.lfilter(b, a, time_series_2)
+
+nyq = 0.5 * 360
+low = 0.4 / nyq
+high = 45 / nyq
+sos = signal.butter(3, [low, high], analog=False, btype='band', output='sos')
+filtered_time_series_1 = signal.sosfilt(sos, time_series_1)
+filtered_time_series_2 = signal.sosfilt(sos, time_series_2)
 print("applied filters")
 
 with open(output, "w", newline="") as outfile:
@@ -37,3 +46,9 @@ with open(output, "w", newline="") as outfile:
     for i in range(len(filtered_time_series_1)):
         writer.writerow([sample[i], filtered_time_series_1[i], filtered_time_series_2[i]])
     print("Wrote output")
+
+
+
+
+t=np.linspace(0, 15, 5400)
+x = filtered_time_series_1[0:5400]
